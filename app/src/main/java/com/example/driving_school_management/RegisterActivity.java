2314b9c48d812366bcf491f9a46f016cedfaf503
firas.hojirat <firas.hojirat@radcom.com>
2019-08-17 12:50:24 +0300

    added register/main activities

---
+++ app/src/main/java/com/example/driving_school_management/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.driving_school_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
